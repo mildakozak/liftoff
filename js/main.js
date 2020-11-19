095a5ff2c0520c54752f9fd2c2846d2edb8f020a
milan <milda.kozak@gmail.com>
2020-11-19 10:29:59 +0100

    test

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var mobileMenu = document.getElementById("mobile-menu");
+var nav = document.getElementById("navbar");
+var iconBack = document.getElementById("mobile-menu-back");
+
+/*
+function navbarToggle() {
+  mobileMenu.style.display = "block";
+  iconBack.style.display = "block";
+}
+
+function navbarToggleOff() {
+  mobileMenu.style.display = "";
+}
+*/
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (document.body.scrollTop > 80 || document.documentElement.scrollTop < 80) {
+document.getElementById("navbar").className = "navigation-transparent"
+} else {
+document.getElementById("navbar").className = "navigation";
+}
+}
